66419cbc64d833a92586d6b7bb6f9c40b5f7111b
Kaito Kudou <kaitokudou2468110921@gmail.com>
2023-01-15 13:38:12 +0900

    [add]プロジェクトを作成

---
+++ sample-chatgpt-app/sample-chatgpt-app/sample_chatgpt_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_chatgpt_appApp.swift
+//  sample-chatgpt-app
+//
+//  Created by 工藤 海斗 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct sample_chatgpt_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
